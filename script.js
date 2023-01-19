36c765b306c35f40917b4bd291def324e7d8331a
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-01-19 14:16:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+    let strb = str.tolowerCase()
+    let reverse = strb.split("").reverse().join("")
+    if (reverse === strb)return true;
+    return false;
 }
+
 module.exports = palindrome
