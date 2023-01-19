5d523c7126655b32940d1bdda8db0cff478537e9
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-01-19 14:20:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-    let strb = str.tolowerCase()
+    let strb = str.toLowerCase()
     let reverse = strb.split("").reverse().join("")
     if (reverse === strb)return true;
     return false;
